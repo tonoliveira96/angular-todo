c61dd4e546dfd3f23fb5d6a3e319e2b70bebf161
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-10-27 17:32:54 -0300

    feat: Persiste dados no LocalStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CreateComponent } from './components/create/create.component';
 import { TaskComponent } from './components/task/task.component';
@@ -11,20 +11,42 @@ import { TaskProps } from './types/types';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges {
   title = 'angular-todo';
   tasks: TaskProps[] = [];
+
+  ngOnInit(): void {
+    this.getLocalStorageData();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes["tasks"]){
+      this.saveLocalStorage(this.tasks)
+    }
+  }
+
+  getLocalStorageData() {
+    var localData = localStorage.getItem("@angula-todo");
+
+    if (!localData) return;
+
+    this.tasks = JSON.parse(localData);
+  }
+
+  saveLocalStorage(list: TaskProps[]) {
+    localStorage.setItem("@angula-todo", JSON.stringify(list));
+  }
 
   addToArray(item: TaskProps) {
     this.tasks.push(item);
   }
 
-  completeTask(id: number){
-    this.tasks[id].completed = !this.tasks[id].completed
+  completeTask(id: number) {
+    this.tasks[id].completed = !this.tasks[id].completed;
   }
 
-  deleteTask(id: number){
-    this.tasks.splice(id, 1)
+  deleteTask(id: number) {
+    this.tasks.splice(id, 1);
   }
 
   getAmount() {
